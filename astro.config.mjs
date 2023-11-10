9a31539111ca26ca8050bd75689744a0d180b835
Andrew Ball <andrew@nordicgamelab.org>
2023-11-10 16:48:04 -0500

    added feedback

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,29 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [starlight({
+    title: 'Nordic Game Lab Software Documentation',
+	logo: {
+		src: './src/assets/nordicgamelab.webp',
+		replacesTitle: true,
+	},
+	components: {
+        // Override the default `SocialIcons` component.
+        Pagination: './src/components/PrevNextLinks.astro',
+      },
+    sidebar: [{
+      label: 'Guides',
+      items: [
+      // Each item here is one entry in the navigation menu.
+      {
+        label: 'Example Guide',
+        link: '/guides/example/'
+      }]
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+    }]
+  })]
 });
